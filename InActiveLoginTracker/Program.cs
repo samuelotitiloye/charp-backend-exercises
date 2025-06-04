af2164921eaf0e3d09e68f1c03bb843e5d63bacf
Samuel Titiloye <samuelotitiloye@gmail.com>
2025-06-03 23:03:38 -0500

    completer PromptUserForInput logic

--- InActiveLoginTracker/Program.cs
+++ InActiveLoginTracker/Program.cs
@@ -4,18 +4,49 @@ namespace InActiveLoginTracker
     {
         public static ParsedInput PromptUserForInput()
         {
+            string fileName = string.Empty;
+            int expirationThreshold = 0;
+            
             // 1. Ask user for file name
-            string? userInput = Console.ReadLine();
             // 2. Check if file exists — if not, ask again
-            if (File.Exists(userInput))
+            while (true)
             {
-                //
+                Console.WriteLine("Enter the path to the login data file(.json or .csv): ");
+                string? userInput = Console.ReadLine(); // ask user for file name?
+
+                if (!string.IsNullOrEmpty(userInput) && File.Exists(userInput))
+                {
+                    fileName = userInput;
+                    break;// exit loop
+                }
+                else
+                {
+                    Console.WriteLine("File is not found. Please enter a valid file path:");
+                }
             }
 
             // 3. Ask for expiration threshold
-            // 4. Check if it's a valid int > 0 — if not, ask again
+            while (true)
+            {
+                Console.WriteLine("Enter expiration threshold in days (positive number): ");
+                string? thresholdInput = Console.ReadLine();
+                // 4. Check if it's a valid int > 0 — if not, ask again
+                if(int.TryParse(thresholdInput, out int parsedThreshold) && parsedThreshold > 0)
+                {
+                    expirationThreshold = parsedThreshold;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid number. Please enter a positive integer.");
+                }
+            } 
             // 5. Return ParsedInput with both value
-
+            return new ParsedInput
+            {
+                FileName = fileName,
+                ExpirationThreshold = expirationThreshold
+            };
         }
     }
 }
